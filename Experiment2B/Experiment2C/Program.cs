c11863ddef797ffc9f733fdaf3ea8d8696795342
Ethan Zogg <75412780+weebeejeebees@users.noreply.github.com>
2023-08-24 13:00:27 +0800

    Add files via upload

---
+++ Experiment2B/Experiment2C/Program.cs
@@ -0,0 +1,32 @@
+﻿using Experiment2B;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Ethan
+        char firstInitial = 'E';
+        char middleInitial = 'O';
+        char lastInitial = 'Z';
+
+        // Taps
+        char friendFirstInitial = 'A';
+        char friendMiddleInitial = 'A';
+        char friendLastInitial = 'T';
+
+        // Stored initials using string interpolation
+        string ethansInitials = $"{firstInitial}. {middleInitial}. {lastInitial}.";
+        string tapsInitials = $"{friendFirstInitial}. {friendMiddleInitial}. {friendLastInitial}.";
+
+        // calls the method with the paramters of where the initials are stored
+        DisplayMonogram(ethansInitials);
+        DisplayMonogram(tapsInitials);
+  
+    }
+
+    static void DisplayMonogram(string initials)
+    {
+        Console.WriteLine("* " + initials + " *");
+    }
+}
---
+++ Experiment2B/Experiment2C/myClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Experiment2B
+{
+    class myClass
+    {
+        public static void lol(string[] args) {
+            Console.WriteLine("Ethan");
+        }
+    }
+}
